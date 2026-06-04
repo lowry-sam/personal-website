2109f1ac2bb94b44c433a3880500e17bff99a501
lowry-sam <slowry44@me.com>
2026-06-03 20:59:31 -0400

    worked a lot with the resume section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,27 +4,65 @@ import Footer from '../Footer';
 import './index.scss'
 import resumeData from '../../data/resume.json'
 import Spinner from '../Spinner';
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
-const renderResume = (resume) => {
+const Resume = () => {
+    const [ selected, setSelected ] = useState(null);
+        
+        const toggle = (idx) => {
+            if(selected === idx) {
+                return setSelected(null);
+            }
+
+            setSelected(idx);
+        }
+
+    const [opened, setOpened] = useState(false);
+
+    const toggled = (num) => {
+        setOpened(num);
+    }
+
+    const renderResume = (resume) => {
         return (
             <div className='container'>
+                
                 {
-                    resume.map((job, idx) => {
+                    resume.map((job) => {
                         return (
-                            <div key={idx} className='resume-box'>
-                                {/* <img src={port.cover} alt="portfolio" className="portfolio-image"/> */}
-                                <div className="content">
-                                    <p className="title">{job.title}</p>
-                                    <h4 className="description">{job.description}</h4>
-                                    <h4 className="description">{job.company}</h4>
-                                    <h4 className="tags">{job.tags}</h4>
-                                    <h4 className="dates">{job.startDate} to {job.endDate}</h4>
-                                    {/* <button
-                                    className="btn"
-                                    onClick={() => window.open(job.url)}
-                                    >View</button> */}
-                                </div>
+                            <div className="accordion">
+                                    <div key={job.title} className={selected === job.title ? 'resume-box selected' : 'resume-box not-selected'}>
+                                        <div  > 
+                                            {/* <img src={port.cover} alt="portfolio" className="portfolio-image"/> */}
+                                            
+                                            <div className='title-box' onClick={()=>toggle(job.title)}>
+                                                <div className="title"> 
+                                                    <h4 className="position" >{job.title} </h4>
+                                                    <h4 className="company">{job.company}</h4>
+                                                </div>
+                                                <span className='arrow'>{selected === job.title ? '↓' : '↑' }</span>
+                                            </div>
+                                            <div className={selected === job.title ? 'content-show' : 'content'} >
+                                                <div className="location"><h4 className="location">Location:</h4> {job.location}</div>
+                                                <h4 className='description'>Description:  
+                                                <ul className='description'>
+                                                    {
+                                                        job.description.map((item) => (<li> {item} </li>)
+                                                    )}
+                                                    </ul>
+                                                </h4>
+                                                <h4 className='dates'>From <h4>{job.startDate}</h4> to <h4>{job.endDate}</h4></h4>
+                                                <h4 className='tags'>Tags:  
+                                                <ul className='tag-box'>
+                                                    {
+                                                        job.tags.map((tag) => (<li> <span>{tag}</span> </li>)
+                                                    )}
+                                                    </ul>
+                                                </h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                
                             </div>
                         )
                     })
@@ -32,49 +70,34 @@ const renderResume = (resume) => {
             </div>
         );
     }
-
-const Resume = () => {
+    
 
     return (
         <>
-       
-        
-        <div className='resume'>
-            <div className='box'> 
-                <h1>Hello!</h1>
-                <div className="render-resume">
-                    {renderResume(resumeData.resume)}
+            <div className='resume'>
+                <h1>Education, Work Experiences, and Skills</h1>
+                <div className="title-description">
+                    <p>Below you will find my relevant education, work experiences, and skills and abilities.</p>
+                    <p>Use the dropdown menus to navigate!</p>
                 </div>
-            
+                <div className='box'> 
+                        <div className="render-resume">
+                            <h2 className='resume-title'>Software Engineering & Data Analytics</h2>
+                            {renderResume(resumeData.resumeTech)}
+                        </div>
+                        <div className="render-resume">
+                            <h2 className='resume-title'>Training & Education</h2>
+                            {renderResume(resumeData.resumeTraining)}
+                        </div>
+                        <div className="render-resume">
+                            <h2 className='resume-title'>Customer Service</h2>
+                            {renderResume(resumeData.resumeService)}
+                    </div>
+                    
+                </div>
+                <Footer/>
             </div>
-           
-            
-            {/* <ul>
-                <li>helisdjflsdf</li>
-                <li>helisdjflsdf</li>
-                <li>helisdjflsdf</li>
-
-
-
-
-                v<li>helisdjflsdf</li>
-                <li>helisdjflsdf</li>
-                <li>helisdjflsdf</li>
-                <li>helisdjflsdf</li>
-                <li>helisdjflsdf</li>
-                <li>helisdjflsdf</li>
-                <li>helisdjflsdf</li>
-                <li>helisdjflsdf</li>
-                <li>helisdjflsdf</li>
-                <li>helisdjflsdf</li>
-            </ul> */}
-            <Footer/>
-        </div>
-       
-        
-        
-        
-        <Loader type='ball-scale-ripple-multiple'/>
+            <Loader type='ball-scale-ripple-multiple'/>
         </>
     )
 }
